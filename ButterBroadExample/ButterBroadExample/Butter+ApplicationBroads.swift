42c5f8a4653f3989e2dad428c16301c78bf40290
nik <nikolay.tynin@rosberry.com>
2020-02-06 16:50:34 +0600

    update for dependencies

--- ButterBroadExample/ButterBroadExample/Butter+ApplicationBroads.swift
+++ ButterBroadExample/ButterBroadExample/Butter+ApplicationBroads.swift
@@ -7,12 +7,12 @@
 
 import ButterBroad
 import AnalogBroad
-import FaceBookBroad
+import FacebookBroad
 import FirebaseBroad
 
 extension Butter {
     static let analog: AnalogBroad = .init()
-    static let facebook: FaceBookBroad = .init()
+    static let facebook: FacebookBroad = .init()
     static let firebase: FirebaseBroad = .init()
     static let common: Butter = .init(broads: analog, facebook, firebase)
 }
